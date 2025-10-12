1eeb9eba9a2335e47c15eed8ddf4e1054ffa687c
BiBoyang <biboyang0@126.com>
2025-10-12 23:12:52 +0800

    fix

---
+++ SwiftUIDemo/SwiftUIDemo_03/SwiftUIDemo_03/SwiftUIDemo_03App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemo_03App.swift
+//  SwiftUIDemo_03
+//
+//  Created by Boyang on 2025/10/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemo_03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
